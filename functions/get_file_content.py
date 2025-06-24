84c7c3c749795f533c0861f3b9e577ae0217dd45
Nico Niermann <mail@niconiermann.de>
2025-06-25 01:21:40 +0200

    added function for file content output

---
+++ functions/get_file_content.py
@@ -0,0 +1,21 @@
+import os
+
+def get_file_content(working_directory, file_path=None):
+    try:
+        if file_path == None or file_path == ".":
+            file_abspath = os.path.abspath(working_directory) + "/"
+        elif file_path.startswith("/") or file_path.startswith("../"):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        else:
+            file_abspath = os.path.abspath(working_directory) + "/" + file_path
+
+        if not os.path.isfile(file_abspath):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        else:
+            with open(file_abspath) as open_file:
+                file_content_string = open_file.read(10000)
+                if len(open_file.read(10001)) == 10001:
+                    file_content_string += f'\n[...File "{file_path}" truncated at 10000 characters]'
+            return file_content_string  
+    except Exception as e:
+        print("Error:" + e)
--- tests.py
+++ tests.py
@@ -1,7 +1,13 @@
-from functions.get_files_info import get_files_info
+# from functions.get_files_info import get_files_info
 
-print(get_files_info("calculator", "."))
-print(get_files_info("calculator", "pkg"))
-print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "../"))
+# print(get_files_info("calculator", "."))
+# print(get_files_info("calculator", "pkg"))
+# print(get_files_info("calculator", "/bin"))
+# print(get_files_info("calculator", "../"))
+
+from functions.get_file_content import get_file_content
+print(get_file_content('calculator', 'lorem.txt'))
+print(get_file_content('calculator', 'main.py'))
+print(get_file_content('calculator', 'pkg/calculator.py'))
+print(get_file_content('calculator', '/bin/cat'))
 
