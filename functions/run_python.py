a13be77b8698acb22322e61d9d12f0acb5336557
zuckeryunge <mail@niconiermann.de>
2025-06-26 00:54:44 +0200

    not working

--- functions/run_python.py
+++ functions/run_python.py
@@ -3,9 +3,16 @@ import subprocess
 
 # function for safely executing things by aigent
 def execute(path_to_exec_file):
-    subprocess.run("p3", path_to_exec_file, capture_output=True, timeout=30)
+    print(os.path.abspath(path_to_exec_file))
+    outout = subprocess.run(path_to_exec_file, capture_output=True, timeout=30, check=True)
+    print("one")
+    print(outout)
     exec_out = f"STDOUT: {subprocess.CompletedProcess.stdout}"
+    print(exec_out)
+    print("two")
     exec_err = f"STDERR: {subprocess.CompletedProcess.stderr}"
+    print(exec_err)
+    print("three")
 
     return exec_out, exec_err
 
@@ -15,18 +22,25 @@ def execute(path_to_exec_file):
 def run_python_file(working_directory, file_path):
     try:
         if file_path.startswith("/") or file_path.startswith("../"):
+            print("1")
             return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
         else:
             file_abspath = os.path.abspath(working_directory) + "/" + file_path
+            print("2")
 
         if os.path.isdir(file_abspath) or file_path == None or file_path == "." or file_path == "":
+            print("3")
             return f'Error: No path given or is a directory: "{file_path}"'
         else:
             if not os.path.exists(file_abspath):
+                print("4")
                 return f'Error: File "{file_path}" not found.' 
             elif not file_abspath.endswith(".py"):
+                print("5")
                 return f'Error: "{file_path}" is not a Python file.'
             else:
-                return execute(file_abspath)
+                print("6")
+                execute(os.path.join(working_directory, file_path))
     except Exception as e:
+        print("eee")
         print(f"Error: executing Python file: {e}")
